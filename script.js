d26360fb7177ca457ae8e1e433e51012597286f1
Vikash Prem Sharma <vikashkumarukg977@gmail.com>
2023-08-18 20:22:14 +0530

    Rename file name

--- script.js
+++
@@ -1,8 +0,0 @@
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-const body = document.body;
-const container = document.querySelector('.container');
-
-darkModeToggle.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-  container.classList.toggle('dark-mode');
-});
